dcfda1564ab457bf1d8867ec8a922ad9d6339370
Banya Kim <kimbanya@gmail.com>
2020-11-26 22:28:18 +0900

    Edit min함수

--- banyakim/function/min함수.py
+++ banyakim/function/min함수.py
@@ -5,6 +5,22 @@ num1, num2 = map(int, input().split(' '));
 # print(num1, num2)
 
 def min(num1, num2):
+	result = 0
+	if num1 > num2:
+		result = num2
+	elif num1 < num2:
+		result =num1
+	else:
+		print('cannot calculate!')
+	print(result)
+
+min(num1, num2)
+
+
+
+num1, num2 = map(int, input().split(' '));
+
+def min(num1, num2):
 	if num1 > num2:
 		print(num2)
 	elif num1 < num2:
